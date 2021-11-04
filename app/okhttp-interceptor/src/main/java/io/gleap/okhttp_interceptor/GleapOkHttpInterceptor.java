89d5723e7aa5cd5f2668d31b15059b0cf068e4d3
niklasfessler <niklas@boehlerbrothers.como>
2021-11-04 10:36:04 +0100

    added deploy

--- app/okhttp-interceptor/src/main/java/io/gleap/okhttp_interceptor/GleapOkHttpInterceptor.java
+++ app/okhttp-interceptor/src/main/java/io/gleap/okhttp_interceptor/GleapOkHttpInterceptor.java
@@ -12,6 +12,7 @@ import java.util.Date;
 
 import io.gleap.Gleap;
 import io.gleap.RequestType;
+import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -23,38 +24,63 @@ public class GleapOkHttpInterceptor implements Interceptor {
     public Response intercept(@NonNull Chain chain) throws IOException {
         Request request = chain.request();
         Buffer buffer = new Buffer();
-
+        Headers headers = request.headers();
+        JSONObject headersReqObj = new JSONObject();
+        for (String name :
+                headers.names()) {
+            String value = request.headers().get(name);
+            try {
+                headersReqObj.put(name, value);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
         JSONObject resquestObj = new JSONObject();
         try {
-            if(request.body() != null) {
+            if (request.body() != null) {
                 request.body().writeTo(buffer);
                 String responseBody = buffer.readUtf8();
 
-                if(isJSONValid(responseBody)){
+                if (isJSONValid(responseBody)) {
                     resquestObj = new JSONObject(responseBody);
-                }else {
-                    resquestObj.put("data",buffer.readUtf8());
+                } else {
+                    resquestObj.put("data", buffer.readUtf8());
                 }
 
             }
+            resquestObj.put("headers", headersReqObj);
         } catch (Exception e) {
 
         }
-        Date start = new Date();
-        Response response = chain.proceed(request);
-        Date end = new Date();
 
-        long duration = start.getTime() - end.getTime();
+        Response response = chain.proceed(request);
+
+        long start = response.sentRequestAtMillis();
+        long end = response.receivedResponseAtMillis();
+        long duration = end - start;
+        JSONObject headersResObj = new JSONObject();
+        for (String name :
+                response.headers().names()) {
+            String value = response.headers().get(name);
+            try {
+                headersResObj.put(name, value);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
         JSONObject responseObj = new JSONObject();
         String responseString = response.body().string();
         try {
-            if(isJSONValid(responseString)){
+            if (isJSONValid(responseString)) {
                 responseObj = new JSONObject(responseString);
-            }else if(isJSONArrayValid(responseString)) {
+            } else if (isJSONArrayValid(responseString)) {
                 responseObj.put("data", new JSONArray(responseString));
-            }else {
+            } else {
                 responseObj.put("data", responseString);
             }
+            responseObj.put("headers", headersResObj);
+            resquestObj.put("success", response.isSuccessful());
         } catch (JSONException e) {
         }
         RequestType requestType = RequestType.GET;
@@ -72,11 +98,11 @@ public class GleapOkHttpInterceptor implements Interceptor {
                 requestType = RequestType.PATCH;
                 break;
         }
-        Gleap.getInstance().logNetwork(request.url().toString(),  requestType,response.code(), (int)duration, resquestObj, responseObj);
+        Gleap.getInstance().logNetwork(request.url().toString(), requestType, response.code(), (int) duration, resquestObj, responseObj);
         return response;
     }
 
-    public boolean isJSONValid(String jsonInString ) {
+    public boolean isJSONValid(String jsonInString) {
         try {
             JSONObject jsonObject = new JSONObject(jsonInString);
             return true;
@@ -85,7 +111,7 @@ public class GleapOkHttpInterceptor implements Interceptor {
         }
     }
 
-    public boolean isJSONArrayValid(String jsonInString ) {
+    public boolean isJSONArrayValid(String jsonInString) {
         try {
             JSONArray jsonObject = new JSONArray(jsonInString);
             return true;
