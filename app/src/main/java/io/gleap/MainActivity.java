936a8d3d16784a0f6a12587684648142409e70de
niklasfessler <niklas@boehlerbrothers.como>
2021-11-05 11:12:43 +0100

    cleanup

--- app/src/main/java/io/gleap/MainActivity.java
+++ app/src/main/java/io/gleap/MainActivity.java
@@ -23,58 +23,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    Gleap.getInstance().startFeedbackFlow();
-                } catch (GleapNotInitialisedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new GleapOkHttpInterceptor())
-                .build();
-
-        Request request = new Request.Builder()
-                .url("https://6170243923781c00172898a1.mockapi.io/users")
-                .header("User-Agent", "OkHttp Example")
-                .build();
-
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                response.body().close();
-            }
-        });
-
-        final String json = "{\"id\":1,\"name\":\"John\"}";
-
-        final RequestBody body = RequestBody.create(
-                MediaType.parse("application/json"), json);
-
-        Request request2 = new Request.Builder()
-                .url("https://www.google.com/users/multipart")
-                .post(body)
-                .build();
-
-        client.newCall(request2).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-
-            }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-
-            }
-        });
     }
 }
--- app/src/main/java/io/gleap/MainApplication.java
+++ app/src/main/java/io/gleap/MainApplication.java
@@ -6,6 +6,5 @@ public class MainApplication  extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Gleap.initialize("7qnF4SaW8daomwcBLdXAd8ahlIYJtxos",this);
     }
 }
